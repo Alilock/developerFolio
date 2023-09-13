8a2c5b6cdfcffa708226136f1b574825ebb57bd0
Ali <105427281+Alilock@users.noreply.github.com>
2023-09-13 22:46:05 +0400

    sabah

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -13,16 +13,13 @@ import Podcast from "./podcast/Podcast";
 import Education from "./education/Education";
 import ScrollToTopButton from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
-import email from "../assets/lottie/email";
-
 import Profile from "./profile/Profile";
 import SplashScreen from "./splashScreen/SplashScreen";
-import { contactInfo, illustration, splashScreen } from "../portfolio";
+import { splashScreen } from "../portfolio";
 import { StyleProvider } from "../contexts/StyleContext";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import "./Main.scss";
-import { Fade } from "react-reveal";
-import DisplayLottie from "../components/displayLottie/DisplayLottie";
+import Contact from "./contact/Contact";
 
 const Main = () => {
   const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
@@ -67,61 +64,8 @@ const Main = () => {
             <Twitter />
             <Podcast />
             <Profile />
-            <Fade bottom duration={1000} distance="20px">
-              <div className="main contact-margin-top" id="contact">
-                <div className="contact-div-main">
-                  <div className="contact-header">
-                    <h1 className="heading contact-title">{contactInfo.title}</h1>
-                    <p
-                      className={
-                        isDark
-                          ? "dark-mode contact-subtitle"
-                          : "subTitle contact-subtitle"
-                      }
-                    >
-                      {contactInfo.subtitle}
-                    </p>
-                    <div
-                      className={
-                        isDark ? "dark-mode contact-text-div" : "contact-text-div"
-                      }
-                    >
-                      {contactInfo.number && (
-                        <>
-                          <a
-                            className="contact-detail"
-                            href={"tel:" + contactInfo.number}
-                          >
-                            {contactInfo.number}
-                          </a>
-                          <br />
-                          <br />
-                        </>
-                      )}
-                      <a
-                        className="contact-detail-email"
-                        href={"mailto:" + contactInfo.email_address}
-                      >
-                        {contactInfo.email_address}
-                      </a>
-                      <br />
-                      <br />
-                      {/* <SocialMedia /> */}
-                    </div>
-                  </div>
-                  <div className="contact-image-div">
-                    {illustration.animated ? (
-                      <DisplayLottie animationData={email} />
-                    ) : (
-                      <img
-                        alt="Man working"
-                        src={require("../assets/images/contactMailDark.svg")}
-                      ></img>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </Fade>
+            <Contact />
+            {/* <Footer /> */}
             <ScrollToTopButton />
           </>
         )}
